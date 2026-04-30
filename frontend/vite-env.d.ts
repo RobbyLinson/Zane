9241eddc7574682ed378e63f096a9af67cdde355
RobbyLinson <robbylinson@gmail.com>
2026-04-30 11:24:02 -0400

    Frontend Build Fix

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,5 +1,8 @@
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
 }
 
 interface ImportMeta {
